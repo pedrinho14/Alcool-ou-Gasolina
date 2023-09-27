077a0cb547e662e1d312f5b1ee27443002386e0b
Pedro Lopes <pedrodvl74@gmail.com>
2023-09-27 19:13:51 -0300

    primeiro commit

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,42 +2,68 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var percentual:Double = 0.7
+    private var percentual: Double = 0.7
+    private lateinit var edAlcool: EditText
+    private lateinit var edGasolina: EditText
+    private lateinit var swPercentual: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("PDM23","No onCreate, $percentual")
 
-        val btCalc: Button = findViewById(R.id.btCalcular)
-        btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            Log.d("PDM23","No btCalcular, $percentual")
-        })
+
+        edAlcool = findViewById(R.id.edAlcool)
+        edGasolina = findViewById(R.id.edGasolina)
+        swPercentual = findViewById(R.id.swPercentual)
+        val btCalc = findViewById<Button>(R.id.btCalcular)
+
+
+        if (savedInstanceState != null) {
+            percentual = savedInstanceState.getDouble("percentual", 0.7)
+            swPercentual.isChecked = percentual == 0.7
+        }
+
+
+        swPercentual.setOnCheckedChangeListener { _, isChecked ->
+            percentual = if (isChecked) {
+                swPercentual.text = "75%"
+                0.75
+            } else {
+                swPercentual.text = "70%"
+                0.70
+            }
+        }
+
+
+        btCalc.setOnClickListener {
+            val alcoolText = edAlcool.text.toString()
+            val gasolinaText = edGasolina.text.toString()
+
+            if (alcoolText.isNotEmpty() && gasolinaText.isNotEmpty()) {
+                val alcoolPrice = alcoolText.toDouble()
+                val gasolinaPrice = gasolinaText.toDouble()
+
+                val resultado = if (alcoolPrice / gasolinaPrice <= percentual) {
+                    "Álcool é a melhor escolha!"
+                } else {
+                    "Gasolina é a melhor escolha!"
+                }
+
+                Toast.makeText(this, resultado, Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Por favor preencha os preços dos combustíveis.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-override fun onResume(){
-    super.onResume()
-    Log.d("PDM23","No onResume, $percentual")
-}
-override fun onStart(){
-    super.onStart()
-    Log.d("PDM23","No onResume")
-}
-override fun onPause(){
-    super.onPause()
-    Log.d("PDM23","No onResume")
-}
-override fun onStop(){
-    super.onStop()
-    Log.d("PDM23","No onResume")
-}
-override fun onDestroy(){
-    super.onDestroy()
-    Log.d("PDM23","No onResume")
-}
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putDouble("percentual", percentual)
+    }
 }
